1020acf91dfb819f163b00665d23de255b2a5e89
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2022-10-11 18:56:47 +0530

    Maze

---
+++ Maze.java
@@ -0,0 +1,15 @@
+class Maze{
+    public static void main(String[] args) {
+        System.out.println(Count(3,3));
+    }
+    public static int Count(int r,int c)
+    {
+        if(r==1 || c==1)
+        {
+            return 1;
+        }
+        int left=Count(r-1, c);
+        int right=Count(r, c-1);
+        return left+right;
+    }
+}
