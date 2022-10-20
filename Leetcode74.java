1d0cbfd94ed649b7005985d9a2f7f2b0147f0074
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2022-10-20 23:37:27 +0530

    Leetcode 74

---
+++ Leetcode74.java
@@ -0,0 +1,27 @@
+https://leetcode.com/problems/search-a-2d-matrix/
+
+class Solution {
+    public boolean searchMatrix(int[][] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (BinarySearch(arr[i], 0, arr[i].length-1, key)) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+    public Boolean BinarySearch(int arr[], int start, int end, int key) {
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == key) {
+                return true;
+            } else if (arr[mid] > key) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+}
