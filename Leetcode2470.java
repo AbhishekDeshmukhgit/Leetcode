a047c8117f25c0a6722a379597ee23dcc242a5a8
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2022-11-13 17:45:39 +0530

    Number of Subarrays With LCM Equal to K

---
+++ Leetcode2470.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int subarrayLCM(int[] nums, int k) {
+       int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+           int lcm=1;
+            for(int j=i;j<nums.length;j++)
+            {
+                lcm=LCM(lcm,nums[j]);
+                if(lcm==k)
+                {
+                    count++;
+                }
+                
+            }
+        }
+        return count;
+    }
+    public int GCD(int a,int b)
+    {
+        if(b==0)
+        {
+            return a;
+        }
+        return GCD(b,a%b);
+    }
+    public int LCM(int a,int b)
+    {
+        return (a*b)/GCD(a,b);
+    }
+}
