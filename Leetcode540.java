a33c577f14c7c9d0c9b076b96367107c41bc86df
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2022-11-25 00:01:01 +0530

    Single Element in a Sorted Array

---
+++ Leetcode540.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        for(int i=1;i<nums.length;i=i+2)
+        {
+            if(nums[i]!=nums[i-1])
+            {
+                return nums[i-1];
+            }
+        }
+        return nums[nums.length-1];
+    }
+}
