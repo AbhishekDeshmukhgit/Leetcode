eb92196e6843d34fccd77efba149fedd40ccabb6
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2022-12-08 23:24:29 +0530

    Truncate Sentence

--- Leetcode1816.java
+++ Leetcode1816.java
@@ -10,3 +10,23 @@ class Solution {
         return res;
     }
 }
+
+
+//Optimized Solution
+class Solution {
+    public String truncateSentence(String s, int k) {
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i) == ' ')
+            {
+                k--;
+            }
+            if(k==0)
+            {
+                return s.substring(0,i);
+            }
+        }
+        return s;
+    }
+}
+
