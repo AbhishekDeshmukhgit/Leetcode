dc516c53ca8693a60fd67854019b1e6a9eed28e5
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2023-01-07 17:38:00 +0530

    Maximum Ice Cream Bars

--- Leetcode1833.java
+++ Leetcode1833.java
@@ -2,7 +2,6 @@ class Solution {
     public int maxIceCream(int[] costs, int coins) {
         Arrays.sort(costs);
         int c=0;
-        //[1,1,2,3,4]3
         for(int i=0;i<costs.length;i++)
         {
             if(coins-costs[i]>=0)
@@ -14,6 +13,22 @@ class Solution {
             if(coins==0)
             {
                 return c;
+            }
+        }
+        return c;
+    }
+}
+//Optimised Approach
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int c=0;
+        for(int i=0;i<costs.length;i++)
+        {
+            if(costs[i]<=coins)
+            {
+                coins=coins-costs[i];
+                c++;
             }
         }
         return c;
