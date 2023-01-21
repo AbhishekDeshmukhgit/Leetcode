2052ce570260a99137fbba14820ce15c5114e4dd
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2023-01-21 23:18:22 +0530

    Minimum Common Value

--- Leetcode2540.java
+++ Leetcode2540.java
@@ -1,3 +1,4 @@
+//Naive Approach
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         int res;
@@ -18,6 +19,28 @@ class Solution {
         if(min!=Integer.MAX_VALUE)
         {
             return min;
+        }
+        return -1;
+    }
+}
+
+//Optimised Approach
+class Solution {
+    public int getCommon(int[] nums1, int[] nums2) {
+        int i=0,j=0;
+        while(i<nums1.length && j<nums2.length)
+        {
+            if(nums1[i]==nums2[j])
+            {
+                return nums1[i];
+            }
+            else if(nums1[i]>nums2[j])
+            {
+                j++;
+            }
+            else{
+                i++;
+            }
         }
         return -1;
     }
