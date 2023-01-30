45fa32d811ed8c1453a8a2663b77ffbdda71bcfe
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2023-01-30 19:12:41 +0530

    N-th Tribonacci Number

--- Leetcode1137.java
+++ Leetcode1137.java
@@ -16,3 +16,18 @@ class Solution {
         return c;
     }
 }
+
+
+//Another Approach
+class Solution {
+    public int tribonacci(int n) {
+        int f[]=new int[38];
+        f[0]=0;
+        f[1]=f[2]=1;
+        for(int i=0;i<n-2;i++)
+        {
+            f[i+3] = f[i] + f[i+1] + f[i+2];
+        }
+        return f[n];
+    }
+}
