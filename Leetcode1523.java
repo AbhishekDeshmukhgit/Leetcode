1826b1fcbdd3d12f0158ecb87ba0185e817a42f2
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2023-02-14 00:10:35 +0530

    Count Odd Numbers in an Interval Range

---
+++ Leetcode1523.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countOdds(int low, int high) {
+        //if low is even make it odd
+
+        if(low%2==0)
+        {
+            low++;
+        }
+        return low > high ? 0 : (high - low) / 2 + 1;
+    }
+}
