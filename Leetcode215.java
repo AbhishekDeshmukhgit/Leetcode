aa163bfcff3208518b32b6898c69f225cfa78024
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2023-03-03 20:44:27 +0530

    Kth Largest Element in an Array

--- Leetcode215.java
+++ Leetcode215.java
@@ -4,3 +4,19 @@ class Solution {
         return nums[nums.length-k];
     }
 }
+
+//Using PriorityQueue
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q=new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<nums.length;i++)
+        {
+            q.offer(nums[i]);
+        }
+        for(int i=1;i<k;i++)
+        {
+            q.poll();
+        }
+        return q.peek();
+    }
+}
