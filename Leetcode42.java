5205fe0573381819b93fd15adeef48c0dcbe10a9
Abhishek Deshmukh <abhishekdeshmukh1604@gmail.com>
2023-06-22 19:21:30 +0530

    Trapping Rain Water

---
+++ Leetcode42.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int trap(int[] height) {
+        int left[]=new int [height.length];
+        int right[]=new int[height.length];
+        left[0]=height[0];
+        for(int i=1;i<height.length;i++)
+        {
+            left[i]=Math.max(left[i-1],height[i]);
+        }
+        right[height.length-1]=height[height.length-1];
+        for(int j=height.length-2;j>=0;j--)
+        {
+            right[j]=Math.max(right[j+1],height[j]);
+        }
+        int water[]=new int[height.length];
+        int res=0;
+        for(int i=0;i<height.length;i++)
+        {
+            water[i]=Math.min(left[i],right[i])-height[i];
+            res=res+water[i];
+        }
+        
+        return res;
+    }
+}
